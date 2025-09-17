aea6a0f6a6af9e326584c2b5a07faa2f7cbe1ac4
MdDanish86 <md.danish0086@gmail.com>
2025-09-17 14:36:36 +0530

    6th commit

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "../styles/Projects.css";
+import confluence from "../assets/images/confluence-dashboard.png"
+import Business from "../assets/images/business-dashboard.png"
 
 export default function Projects() {
   return (
@@ -7,6 +9,9 @@ export default function Projects() {
       <h2 className="heading">Projects</h2>
       <div className="project-grid">
         <div className="project-card">
+          <div className="image-wrapper">
+          <img src={Business} alt="Business Consulting Dashboard" className="dashboard-img" />
+          </div>
           <h3 className="p1">Business Consulting</h3>
           <p className="description">Developed bizpartners.site, an India‑wide marketplace where startups can find angel investors. Worked on implementing responsive UI, secure login/register flows, and a pay‑on‑performance listing model to enable transparent interactions.</p>
 
@@ -22,6 +27,10 @@ export default function Projects() {
 
         </div>
         <div className="project-card">
+                    <div className="image-wrapper">
+
+          <img src={confluence} alt="Confluence Dashboard" className="dashboard-img"/>
+          </div>
           <h3 className="p1">Confluence</h3>
           <p className="description">Confluence is a centralized platform where companies can manage and showcase all their projects in one place. I built intuitive dashboards, project listing features, and efficient navigation so that administrators can easily add, edit, and organize projects.</p>
 
